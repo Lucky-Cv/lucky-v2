cc240702b4c22e2ea84f0ad215edf68167790158
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-30 05:41:34 +0200

    update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -7,7 +7,7 @@ let m2 = `
 ≡ _Use estos comandos sin el prefijo_
 
 ▢ Si tienes más  audios   
-envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
+envíame por wa.me/34643694252 el *audio + comando* con en el que responderá
 
 ┌─⊷ *AUDIOS* 
 ▢ Bot
@@ -18,7 +18,7 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
-    conn.sendButton(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, [
+    conn.sendButton(m.chat, m2, '▢ Lucky  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/Lucky-Cv\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Grupos', `${usedPrefix}gpdylux`]
     ],m)
